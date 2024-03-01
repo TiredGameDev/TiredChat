addb7b14200cc4350118e336186a5fe4e5386b43
TiredGameDev <161785656+TiredGameDev@users.noreply.github.com>
2024-03-01 01:48:05 +0000

    still figuring out how to use github -_-. made a basic server tho

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+const port = 3000;
+// DEV = 3000
+// PROD = 443
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {tab_title: "Home"});
+})
+
+
+
+
+
+if (port == 3000) {
+    app.listen(port, () => {
+        console.log("Server started on port 3000\nPress Ctrl+C to exit\nSERVER IS IN DEVELOPMENT MODE\n Open http://localhost:3000 in your browser");
+    })
+}
+
+if (port == 443) {
+    app.listen(port, () => {
+        console.log("Server started on port 443\nPress Ctrl+C to exit\nSERVER IS IN PRODUCTION MODE");
+    })
+}
